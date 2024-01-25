b1c74a89a5d57800e723a819f4482fe9a138be13
Andrii Malyava <odinokiigero@gmail.com>
2024-01-25 13:21:36 +0200

    Beautify Code.

--- ScrapePetitionVoters/Program.cs
+++ ScrapePetitionVoters/Program.cs
@@ -4,20 +4,15 @@ namespace ScrapePetitionVoters
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void SaveFile(string petitionId, object result)
         {
             string basePath = @"E:\MOUNTED\Desktop\";
-            var scraper = new Scraper();
-
-            var petitionId = 216794;
-            var result = scraper.Scrape(petitionId);
-
             var date = DateTime.Now.ToString("G");
             date = date.Replace("/", "_");
             date = date.Replace(":", "_");
             date = date.Replace(" ", "-");
 
-            string fileName = $"{petitionId}_{date}.json";
+            string fileName = $"Petition_{petitionId}_{date}.json";
             var json = JsonConvert.SerializeObject(result);
 
             string fullPath = Path.Join(basePath, fileName);
@@ -26,6 +21,17 @@ namespace ScrapePetitionVoters
                 Directory.CreateDirectory(basePath);
             }
             File.WriteAllText(fullPath, json);
+        }
+
+        static void Main(string[] args)
+        {
+            var scraper = new Scraper();
+
+            var petitionId = 216794;
+            var result = scraper.Scrape(petitionId);
+
+            SaveFile(petitionId.ToString(), result);
+
             while (true)
             {
                 Console.ReadKey();
